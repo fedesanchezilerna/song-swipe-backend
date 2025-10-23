9cbbee7298dc1e7276aef71f0e3cc933c879b6bd
biel-rhlm <bielrari@alumnes.ilerna.com>
2025-10-23 16:33:13 +0200

    Setup initial structure

---
+++ src/main/java/com/example/songswipe/SongSwipeApplication.java
@@ -0,0 +1,13 @@
+package com.example.songswipe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SongSwipeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SongSwipeApplication.class, args);
+        System.out.println("HELLO WORLD");
+    }
+}
---
+++ src/test/java/com/example/songswipe/SongSwipeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.songswipe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SongSwipeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
