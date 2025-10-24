11423747864cf7e13531b2fba59b4a7821e43d00
biel-rhlm <bielrari@alumnes.ilerna.com>
2025-10-24 18:01:17 +0200

    Estructura inicial SpringBoot

---
+++ src/test/java/com/ilerna/song_swipe_backend/SongSwipeBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ilerna.song_swipe_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SongSwipeBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
