1395e243ccda2b862cb719550daa951d18c38dac
biel-rhlm <bielrari@alumnes.ilerna.com>
2025-10-24 19:54:17 +0200

    Spring boot estructura inicial (#6)
    * Estructura inicial SpringBoot
    
    * Estructura inicial SpringBoot

---
+++ src/main/java/com/ilerna/song_swipe_backend/SongSwipeBackendApplication.java
@@ -0,0 +1,13 @@
+package com.ilerna.song_swipe_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SongSwipeBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SongSwipeBackendApplication.class, args);
+	}
+
+}
